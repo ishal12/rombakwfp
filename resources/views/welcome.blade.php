0c221d48e44734048e48092f012dcff6d8ffba56
ishal12 <faishalhendaryawan@gmail.com>
2016-11-21 17:46:19 +0700

    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,8 +16,7 @@
                   <img class="first-slide" src="{{URL::asset('img/1.jpg')}}" alt="First slide">
                   <div class="container">
                     <div class="carousel-caption">
-                      <h1>Example headline.</h1>
-                      <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+                      
                     </div>
                   </div>
                 </div>
@@ -25,8 +24,7 @@
                   <img class="second-slide" src="{{URL::asset('img/1.jpg')}}" alt="Second slide">
                   <div class="container">
                     <div class="carousel-caption">
-                      <h1>Another example headline.</h1>
-                      <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                      
                     </div>
                   </div>
                 </div>
@@ -34,8 +32,7 @@
                   <img class="third-slide" src="{{URL::asset('img/1.jpg')}}" alt="Third slide">
                   <div class="container">
                     <div class="carousel-caption">
-                      <h1>One more for good measure.</h1>
-                      <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                      
                     </div>
                   </div>
                 </div>
@@ -49,6 +46,8 @@
                 <span class="sr-only">Next</span>
               </a>
             </div>
+
+            
         </div>
     </div>
 </div>
